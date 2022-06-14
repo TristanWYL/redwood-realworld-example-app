14fe23a7f4ff893f38d5adef51eef03c49b74739
Tristan <tristan_wyl@hotmail.com>
2022-06-14 17:19:16 +0800

    'validateUniqueness' involves 'interactiveTransactions' of prisma, which causes unexpected error when manipulating the database, so we replace 'validateUniqueness' by introducing a custom Error.

--- api/src/functions/auth.ts
+++ api/src/functions/auth.ts
@@ -101,21 +101,21 @@ export const handler = async (event, context) => {
     //
     // If this returns anything else, it will be returned by the
     // `signUp()` function in the form of: `{ message: 'String here' }`.
-    handler: ({ username, hashedPassword, userAttributes }) => {
+    handler: async ({ username, hashedPassword, userAttributes }) => {
       const { email } = userAttributes
-      return validateUniqueness(
-        'user',
-        { email },
-        { message: 'Your email is already in use.' },
-        () =>
-          db.user.create({
-            data: {
-              email,
-              username,
-              password: hashedPassword,
-            },
-          })
-      )
+      const user = await db.user.findUnique({
+        where: { email },
+      })
+      if (user) {
+        throw new Error('Your email is already in use.')
+      }
+      return db.user.create({
+        data: {
+          email,
+          username,
+          password: hashedPassword,
+        },
+      })
     },
 
     errors: {
